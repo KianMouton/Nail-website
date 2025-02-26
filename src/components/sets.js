4d36b291e828c62b1c2f80144e1f9d8aae65d849
KianMouton <kianton2004@gmail.com>
2025-02-26 12:31:05 +0200

    added new products to the website

--- src/components/sets.js
+++ src/components/sets.js
@@ -7,6 +7,11 @@ import FloralFrench from '../images/Floral French Fantasy.jpg';
 import MidnightBlossom from '../images/Midnight Blossom.jpg';
 import OceanBreese from '../images/Ocean Breese Elegance.jpg';
 import RomanticBlush from '../images/Romantic Blush.jpg';
+import VioletBloom from '../images/Violet Bloom.jpeg';
+import BlueSkies from '../images/Blue skies.jpeg';
+import CherryConfetti from '../images/Cherry Confetti.jpeg';
+import PetalBlush from '../images/Petal Blush.jpeg';
+import ChampagneChic from '../images/Champagne Chic.jpeg';
 import { Link } from 'react-router-dom';
 
 const products = [
@@ -51,9 +56,39 @@ const products = [
       image: RomanticBlush,
       price: 120,
       description: "This set of 10 luxury press-on nails is crafted and designed for a flawless, salon-quality finish in minutes. The set featured here is shaped in Long Stilleto, and every set is meticulously handmade by me to ensure a perfect fit. Get the perfect, customized nails without the wait!"
-   }
+   },
+   {
+      name: "Violet Bloom",
+      image: VioletBloom,
+      price: 120,
+      description: "This set of 10 luxury press-on nails is crafted and designed for a flawless, salon-quality finish in minutes. The set featured here is shaped in Medium Squire, and every set is meticulously handmade by me to ensure a perfect fit. Get the perfect, customized nails without the wait!"
+   },
+   {
+      name: "Blue Skies",
+      image: BlueSkies,
+      price: 120,
+      description: "This set of 10 luxury press-on nails is crafted and designed for a flawless, salon-quality finish in minutes. The set featured here is shaped in XI stiletto, and every set is meticulously handmade by me to ensure a perfect fit. Get the perfect, customized nails without the wait!"
+   },
+   {
+      name: "Cherry Confetti",
+      image: CherryConfetti,
+      price: 120,
+      description: "This set of 10 luxury press-on nails is crafted and designed for a flawless, salon-quality finish in minutes. The set featured here is shaped in Medium almond, and every set is meticulously handmade by me to ensure a perfect fit. Get the perfect, customized nails without the wait!"
+   },
+   {
+      name: "Petal Blush",
+      image: PetalBlush,
+      price: 120,
+      description: "This set of 10 luxury press-on nails is crafted and designed for a flawless, salon-quality finish in minutes. The set featured here is shaped in Short round, and every set is meticulously handmade by me to ensure a perfect fit. Get the perfect, customized nails without the wait!"
+   },
+   {
+      name: "Champagne Chic",
+      image: ChampagneChic,
+      price: 120,
+      description: "This set of 10 luxury press-on nails is crafted and designed for a flawless, salon-quality finish in minutes. The set featured here is shaped in XI coffin, and every set is meticulously handmade by me to ensure a perfect fit. Get the perfect, customized nails without the wait!"
+   },
 ]
-
+//add shapes na die description
 function Sets() {
    return (
        <div className="sets">
